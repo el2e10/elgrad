3e93ca954b704e063838af4781e7002b9104f336
el2e10 <eldhoittangeorge@gmail.com>
2025-01-09 20:31:37 +0530

    Replace python list by numpy array

--- engine_v2.py
+++ engine_v2.py
@@ -1,37 +1,23 @@
 from typing import Union, List, Tuple
 
-class Tensor:
+import numpy as np #type: ignore
 
+class Tensor:
     def __init__(self, data):
-        self.data = data
+        self.data = np.array(data)
         self.shape = []
         self._get_shape(data)
         self._current_index = 0
 
-    def __getitem__(self, indices: Union[List, int, Tuple]):
-        # To handle indexing like t[0]
-        if isinstance(indices, int):
-            return self.data[indices]
-        if not hasattr(indices, "__len__") and not isinstance(indices, int):
-            raise TypeError("Invalid type for index")
-
-        if len(indices) > len(self.shape):
-            raise IndexError("Invalid dimension for the Tensor")
-        if not all([isinstance(x, int) for x in indices]):
-            raise TypeError("Invalid type for index")
-
-        # To handle indexing like t[1, 2, 3]
-        result = self.data
-        for index in indices:
-            result = result[index]
-        return Tensor(result)
+    def __getitem__(self, indices: Union[List, int, Tuple, slice]):
+       return self.data[indices]
 
     def __iter__(self):
         self.current_index = 0
         return self
 
     def __next__(self):
-        if(self.current_index > self.shape[0]-1):
+        if self.current_index > self.shape[0] - 1:
             raise StopIteration
 
         tensor = Tensor(self.data[self.current_index])
@@ -40,11 +26,16 @@ class Tensor:
 
     def __add__(self, other):
         assert self.shape == other.shape, "Different shape"
-        return other
+        result = self.data + other.data
+
+        return Tensor(result)
+
+    def __mul__(self, other):
+        return Tensor(np.matmul(self.data, other.data))
 
     def _get_shape(self, x):
-        if(not hasattr(x, "__len__")):
-            return 
+        if not hasattr(x, "__len__"):
+            return
         else:
             self.shape.append(len(x))
             return self._get_shape(x[0])
@@ -52,23 +43,11 @@ class Tensor:
     def __repr__(self) -> str:
         return f"Tensor(data={self.data}, shape={self.shape})"
 
+
 if __name__ == "__main__":
-    a = Tensor([[[1, 2, 3],[2, 3, 8]], [[4, 8, 9], [4, 7, 9]]])
-    b = Tensor([[4, 5, 7],[2, 3, 5]])
-
-    for i in a:
-        for j in i:
-            print(j)
+    a = Tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    b = Tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    c = Tensor([[1, 2], [3, 4], [5, 6]])
     
-    # result = []
-    # def sum(a, b):
-    #     print(a)
-    #     if(len(a) == len(b) == 1):
-    #         return [x + y for x, y in zip(a,b)]
-    #     else:
-    #         result.append(sum([*a], [*b]))
-    #
-    # sum(a, b)
-    # print(result)
-
+    print(a * c)
 
