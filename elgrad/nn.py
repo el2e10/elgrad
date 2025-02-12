Add bug fix for softmax and relu
- After applying softmax the children's require_grad was False, fixed it
by adding require_grad as True for the numerator calculation
- Using vectorize for calculating relu was return wrong values for N-D
decimal tensor, fixed it by using maximum function of numpy
- Added label parameter to nn.Linear

elgrad/nn.py
@@ -6,10 +6,10 @@ random_num_generator = np.random.default_rng()
 
 class Linear:
 
-    def __init__(self, inputs, outputs):
+    def __init__(self, inputs, outputs, label=""):
         a = np.sqrt(6/(inputs + outputs))
-        self.w = Tensor(random_num_generator.uniform(-a, a, size=(outputs, inputs)), require_grad=True)
-        self.b = Tensor(random_num_generator.uniform(-1, 1, 1), require_grad=True)
+        self.w = Tensor(random_num_generator.uniform(-a, a, size=(outputs, inputs)), require_grad=True, label=f"{label}-w")
+        self.b = Tensor(random_num_generator.uniform(-1, 1, 1), require_grad=True, label=f"{label}-b")
 
     def __call__(self, input: Tensor) -> Tensor:
         input = Tensor(input) if not isinstance(input, Tensor) else input

elgrad/tensor.py
@@ -267,19 +267,19 @@ class Tensor:
         return output
 
     def softmax(self, dim=0):
-        numerator = Tensor(e) ** self
+        numerator = Tensor(e, require_grad=True) ** self
         denominator = numerator.sum(dim, keepdims=True)
         output = numerator/denominator
         output.require_grad = True
         return output
 
     def relu(self):
-        _relu = np.vectorize(lambda x: 0 if x < 0 else x)
-        output = Tensor(_relu(self.data), children=(self, ), label="ReLU")
+        _relu_out = np.maximum(self.data, 0)
+        output = Tensor(_relu_out, children=(self, ), label="ReLU")
 
         def backward():
-            func = np.vectorize(lambda x: 0 if x <= 0 else 1)
-            self.grad += Tensor(func(output.data)) * output.grad
+            _grad_out = (output.data > 0).astype(float)
+            self.grad += Tensor(_grad_out) * output.grad
         output._backward = backward
 
         return output

test/tensor_test.py
@@ -1,5 +1,6 @@
 import pytest  # type: ignore
 from elgrad import Tensor, BroadcastError  # type: ignore
+random_num_generator = np.random.default_rng()
 
 class TestReLU:
     def relu_grad(self, x: Tensor):
@@ -8,10 +9,12 @@ class TestReLU:
         d.backward()
         return x.grad
 
+        
     def test_one(self):
         a = Tensor([1, 2, 3], require_grad=True)
         a_grad = self.relu_grad(a)
         a_grad_expected = Tensor([1, 1, 1])
+        print(a_grad)
         assert Tensor.array_equal(a_grad, a_grad_expected)
 
     def test_two(self):
@@ -25,8 +28,6 @@ class TestReLU:
         a_grad = self.relu_grad(a)
         a_grad_expected = Tensor([[1, 0, 1], [1, 1, 1]])
         assert Tensor.array_equal(a_grad, a_grad_expected)
-
-
 
 class TestSoftmax:
     def softmax_grad(self, x: Tensor, dim: int):