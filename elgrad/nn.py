Fix softmax and complete iris example
- Fixed gradient calculation of softmax
- Added exp() and test

elgrad/nn.py
@@ -10,7 +10,7 @@ class Linear:
         a = np.sqrt(6/(inputs + outputs))
         self.label = label
         self.w = Tensor(random_num_generator.uniform(-a, a, size=(outputs, inputs)), require_grad=True, label=f"{label}-w")
-        self.b = Tensor(random_num_generator.uniform(-a, a, 1), require_grad=True, label=f"{label}-b")
+        self.b = Tensor(random_num_generator.uniform(-a, a, size=(outputs, )), require_grad=True, label=f"{label}-b")
 
     def __call__(self, input: Tensor) -> Tensor:
         input = Tensor(input) if not isinstance(input, Tensor) else input
@@ -19,8 +19,8 @@ class Linear:
         return output
 
     def learn(self, lr):
-        self.w.data = self.w.data + -lr * self.w.grad.data #type: ignore
-        self.b.data = self.b.data + -lr * self.b.grad.data #type: ignore
+        self.w.data = self.w.data - lr * self.w.grad.data #type: ignore
+        self.b.data = self.b.data - lr * self.b.grad.data #type: ignore
 
     def zero_grad(self):
         self.w.zero_grad()

elgrad/tensor.py
@@ -266,7 +266,7 @@ class Tensor:
         # Check if we the two tensors can be broadcasted. Raise a BroadcastError exception if not possible
         Tensor.can_broadcast(self, other)
 
-        output = Tensor(self.data / other.data, label="Mul", children=(self, other))
+        output = Tensor(self.data / other.data, label="Div", children=(self, other))
 
         def backward():
             if self.require_grad:
@@ -322,21 +322,26 @@ class Tensor:
         def backward():
             # Sum function always assing 1 as gradient to each element
             if self.require_grad:
-                self.grad += Tensor.ones(self.shape)
+                self.grad += Tensor.ones(self.shape) * output.grad
+
+        output._backward = backward
+
+        return output
+
+    def exp(self):
+        output = Tensor(np.exp(self.data), children=(self, ), label="Exp")
+
+        def backward():
+            if self.require_grad:
+                self.grad += Tensor(output.data) * output.grad
 
         output._backward = backward
 
         return output
 
     def softmax(self, dim=0):
-        numerator = Tensor(e, require_grad=True) ** self
-        # print("Numerator is ", numerator)
-        numerator.label = "Softmax-numerator"
-
-        denominator = numerator.sum(dim, keepdims=True)
-        denominator.label = "Softmax-denominator"
-
-        output = numerator / denominator
+        numerator = self.exp()
+        output = numerator / numerator.sum(dim, keepdims=True)
         output.label = "Softmax"
         return output
 
@@ -462,9 +467,3 @@ class Tensor:
 
 if __name__ == "__main__":
     pass
-    a = Tensor([1, 2, 3, 4], require_grad=True, label="Input - A")
-    b = a.softmax()
-    ll = b
-    c = ll.sum()
-    c.backward()
-    print(a)

examples/iris/iris.py
@@ -1,9 +1,11 @@
 import sys
+import os
 from csv import DictReader
 from typing import List
-sys.path.append("/Users/eldhoittangeorge/Business/ML/Projects/elgrad/")
 
-import numpy as np #type: ignore
+sys.path.append(os.getcwd())
+
+import numpy as np  # type: ignore
 
 from elgrad import Tensor
 from elgrad import Linear
@@ -11,52 +13,59 @@ from elgrad import Linear
 
 EPOCHS = 25
 data = []
-with open("/Users/eldhoittangeorge/Business/ML/Projects/elgrad/examples/iris/data.csv", 'r') as fp:
+with open("examples/iris/data.csv", "r") as fp:
     csv_fp = DictReader(fp)
     for row in csv_fp:
         data.append(row)
 
-def convert_label_to_one_hot_encoding(dataset: List[dict], label_colname:str):
-    unique_labels: list[str] = [] 
+
+def convert_label_to_one_hot_encoding(dataset: List[dict], label_colname: str):
+    unique_labels: list[str] = []
     result: list[list[int]] = []
     for row in dataset:
-        if(row[label_colname] not in unique_labels):
+        if row[label_colname] not in unique_labels:
             unique_labels.append(row[label_colname])
 
     length = len(unique_labels)
     for row in dataset:
         position = unique_labels.index(row[label_colname])
-        result.append([0 if position == x else 1 for x in range(length)])
+        result.append([1 if position == x else 0 for x in range(length)])
 
     return result
+
 
 def extract_inputs(dataset: List[dict]):
     result: List[list[float]] = []
     for row in dataset:
-        result.append([row["SepalLengthCm"], row["SepalWidthCm"], row["PetalLengthCm"], row["PetalWidthCm"]])
-
+        result.append(
+            [
+                row["SepalLengthCm"],
+                row["SepalWidthCm"],
+                row["PetalLengthCm"],
+                row["PetalWidthCm"],
+            ]
+        )
     return result
+
 
 def normalize_inputs(t: List[List]):
     t = np.array(t, dtype="float64")
     mean = np.mean(t, axis=0)
     std = np.std(t, axis=0)
-    result = (t - mean)/(std + 1e-8)
+    result = (t - mean) / (std + 1e-6)
     return result
 
 
-label = convert_label_to_one_hot_encoding(data, "Species") 
+label = convert_label_to_one_hot_encoding(data, "Species")
 y = Tensor(label, label="Y")
 normalized_data = normalize_inputs(extract_inputs(data))
-x = Tensor(normalized_data, require_grad=True)
-
+x = Tensor(normalized_data, label="X")
 
 layer1 = Linear(4, 10, label="Layer 1")
 layer2 = Linear(10, 20, label="Layer 2")
 layer3 = Linear(20, 3, label="Layer 3")
 
-
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.03
 for i in range(EPOCHS):
     x1 = layer1(x)
     z1 = x1.relu()
@@ -65,11 +74,10 @@ for i in range(EPOCHS):
     z2 = x2.relu()
 
     x3 = layer3(z2)
-    z3 = x3.softmax()
+    z3 = x3.softmax(dim=1)
 
-    # z4.require_grad = True
-
-    loss = (y * x3.log()).sum()
+    loss = -((y * z3.log()).sum() / 150)
+    loss.label = "Loss"
     print(f"Loss at {i}th step is {loss}")
 
     layer1.zero_grad()
@@ -81,5 +89,3 @@ for i in range(EPOCHS):
     layer1.learn(LEARNING_RATE)
     layer2.learn(LEARNING_RATE)
     layer3.learn(LEARNING_RATE)
-
-